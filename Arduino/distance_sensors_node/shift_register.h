d7f63c9d81b66b1381e0e9970eb84ff3c9bc6d77
Combinacijus <combinacijus@gmail.com>
2019-08-15 12:35:23 +0300

    shift register library done

--- Arduino/distance_sensors_node/shift_register.h
+++ Arduino/distance_sensors_node/shift_register.h
@@ -1,32 +1,44 @@
 /*
-<--- Notch left
-\/ - usually low,  /\ - usually high, MR - Master Reset, QE - Output Enable
- 1    2   3    4    5     6    7     8
- VCC/\ Q0 DATA OE\/ LATCH CLOCK MR/\  Q7'
- ----------------------------------------
- )                                      |
- ----------------------------------------
- Q1   Q2  Q3   Q4   Q5    Q6   Q7   GND\/
-VCC --- 10uF Capacitor --- GND
-2nd Register: 1.Q7' to 2.DATA. Linked: VCC QE LATCH CLOCK MR
+	74HC595N shift register library
+	Pin values are hardcoded with #define for FastGPIO to work
+	Pin values can be change to any pins
+
+	\/ - usually low,  /\ - usually high, MR - Master Reset, QE - Output Enable, --- - Wire
+
+	<--- Notch left
+	1    2   3    4    5     6    7     8
+	VCC/\ Q0 DATA QE\/ LATCH CLOCK MR/\  Q7'
+	----------------------------------------
+	)                                      |
+	----------------------------------------
+	Q1   Q2   Q3   Q4   Q5    Q6   Q7   GND\/
+
+	Also connect: VCC --- 10uF Capacitor --- GND
+
+	Linking 2nd Register: 1.Q7' --- 2.DATA. Linked: VCC QE LATCH CLOCK MR GND
 */
 
 #pragma once
 
 #include <FastGPIO.h>
 
-#define PIN_DATA = 26;
-#define PIN_CLOCK = 25;
-#define PIN_LATCH = 32;
-#define PIN_MR = 35;
-#define PIN_BTN_IN = 35;
+#define PIN_DATA A0
+#define PIN_LATCH A1
+#define PIN_CLOCK A2
+#define PIN_MR A3	// Master reset pin
+#define PIN_IN 13	// Input pin (multiplex for all sensors)
+// #define DEBUG // Uncomment for debugging
 
 class ShiftReg
 {
 public:
-	ShiftReg();
-	void update();
+	ShiftReg(int _bytes_count = 1);
+	void clear();
 	void write_bit(byte _bit);
-	void write_byte(byte _data, bool _reversed = false);
-	int read_byte(int _bytes_num = 1);
+	void write_single_bit(byte _bit);
+	void write_all(int _data, bool _reversed = false);
+	int read_all();
+	// void write_from_serial();
+
+	uint8_t bytes_count = 1; // Number of shift register
 };
