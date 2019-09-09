36516a7dbb1836c9eb7177abb40554569613835c
Combinacijus <combinacijus@gmail.com>
2019-09-09 15:52:04 +0300

    Imu visualization

--- Arduino/odometry_node/odometry_node.ino
+++ Arduino/odometry_node/odometry_node.ino
@@ -24,20 +24,20 @@ void setup()
 
 void loop()
 {
-  // static int c = 0;
-  // encoder_update();
-  // ++c;
+   static int c = 0;
+   encoder_update();
+   ++c;
 
-  // if (c > 1000)
-  // {
-  //   Serial.println(position);
-  //   c = 0;
-  // }
+   if (c > 100)
+   {
+     Serial.println(position);
+     c = 0;
+   }
 
-  Serial.print(analogRead(pin_a));
-  Serial.print(" ");
-  Serial.println(analogRead(pin_b));
-  delay(10);
+//  Serial.print(analogRead(pin_a));
+//  Serial.print(" ");
+//  Serial.println(analogRead(pin_b));
+//  delay(1);
 }
 
 void encoder_setup(int _pin_a, int _pin_b)
@@ -51,13 +51,16 @@ void encoder_setup(int _pin_a, int _pin_b)
 
 void encoder_update()
 {
-	state_a = digitalRead(pin_a); // Reads the "current" state of the outputA
+	//state_a = digitalRead(pin_a); // Reads the "current" state of the outputA
+  state_a = analogRead(pin_a) > 300? 1 : 0; // Reads the "current" state of the outputA
 	// If the previous and the current state of the outputA are different, that means a Pulse has occured
 	if (state_a != state_a_old)
 	{
 		speed = 1000000.0 / (micros() - last_update_time);
 		// If the A state is different to the B state, that means the encoder is rotating clockwise
-		if (digitalRead(pin_b) != state_a)
+    int state_b = analogRead(pin_b) > 300? 0 : 1;
+//    Serial.println(String(state_a) + " " + String(state_b));
+		if (state_b != state_a)
 		{
 			++position;
 		}
@@ -75,7 +78,7 @@ void encoder_update()
 	}
 	else
 	{
-		speed *= 0.95;
+//		speed *= 0.95;
 	}
 
 	state_a_old = state_a; // Updates the previous state of the outputA with the current state
--- ROS/samana_ws/build/catkin_generated/stamps/Project/_setup_util.py.stamp
+++ ROS/samana_ws/build/catkin_generated/stamps/Project/_setup_util.py.stamp
@@ -264,7 +264,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = '/home/combinacijus/Documents/SamanaAutonomousRobot/ROS/samana_ws/devel;/opt/ros/melodic'.split(';')
+            CMAKE_PREFIX_PATH = '/home/combinacijus/Documents/ROS/learning_tf_ws/devel;/home/combinacijus/Documents/SamanaAutonomousRobot/ROS/samana_ws/devel;/opt/ros/melodic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
