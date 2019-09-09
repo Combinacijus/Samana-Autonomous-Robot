3d07f2e23db24a9a983b72f10f66c3d1c116a80b
Combinacijus <combinacijus@gmail.com>
2019-09-09 15:52:04 +0300

    Imu visualization

---
+++ ROS/samana_ws/build/lib/rviz_satellite/src/detail/moc_TileDownloader.cpp
@@ -0,0 +1,116 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'TileDownloader.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.5)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../../../src/lib/rviz_satellite/src/detail/TileDownloader.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'TileDownloader.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.5. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_detail__TileDownloader_t {
+    QByteArrayData data[5];
+    char stringdata0[62];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_detail__TileDownloader_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_detail__TileDownloader_t qt_meta_stringdata_detail__TileDownloader = {
+    {
+QT_MOC_LITERAL(0, 0, 22), // "detail::TileDownloader"
+QT_MOC_LITERAL(1, 23, 16), // "downloadFinished"
+QT_MOC_LITERAL(2, 40, 0), // ""
+QT_MOC_LITERAL(3, 41, 14), // "QNetworkReply*"
+QT_MOC_LITERAL(4, 56, 5) // "reply"
+
+    },
+    "detail::TileDownloader\0downloadFinished\0"
+    "\0QNetworkReply*\0reply"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_detail__TileDownloader[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   19,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+
+       0        // eod
+};
+
+void detail::TileDownloader::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        TileDownloader *_t = static_cast<TileDownloader *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->downloadFinished((*reinterpret_cast< QNetworkReply*(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject detail::TileDownloader::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_detail__TileDownloader.data,
+      qt_meta_data_detail__TileDownloader,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *detail::TileDownloader::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *detail::TileDownloader::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_detail__TileDownloader.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int detail::TileDownloader::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
