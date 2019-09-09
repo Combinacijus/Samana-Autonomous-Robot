2dc379d3796b7cf36788d6de37d89ff033a75044
Combinacijus <combinacijus@gmail.com>
2019-09-09 15:52:04 +0300

    Imu visualization

--- ROS/samana_ws/build/catkin_generated/order_packages.py
+++ ROS/samana_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/combinacijus/Documents/SamanaAutonomousRobot/ROS/samana_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/combinacijus/Documents/SamanaAutonomousRobot/ROS/samana_ws/devel;/opt/ros/melodic".split(';') if "/home/combinacijus/Documents/SamanaAutonomousRobot/ROS/samana_ws/devel;/opt/ros/melodic" != "" else []
+underlay_workspaces = "/home/combinacijus/Documents/ROS/learning_tf_ws/devel;/home/combinacijus/Documents/SamanaAutonomousRobot/ROS/samana_ws/devel;/opt/ros/melodic".split(';') if "/home/combinacijus/Documents/ROS/learning_tf_ws/devel;/home/combinacijus/Documents/SamanaAutonomousRobot/ROS/samana_ws/devel;/opt/ros/melodic" != "" else []
---
+++ ROS/samana_ws/build/rviz_imu_plugin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/combinacijus/Documents/SamanaAutonomousRobot/ROS/samana_ws/src/rviz_imu_plugin
+# Build directory: /home/combinacijus/Documents/SamanaAutonomousRobot/ROS/samana_ws/build/rviz_imu_plugin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
