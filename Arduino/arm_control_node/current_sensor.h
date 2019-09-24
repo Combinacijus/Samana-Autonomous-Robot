97a01709b8b55dd9e6d556e1d40fb86ae3da5299
Combinacijus <combinacijus@gmail.com>
2019-09-24 17:00:34 +0300

    Calibration function and some changes

--- Arduino/arm_control_node/current_sensor.h
+++ Arduino/arm_control_node/current_sensor.h
@@ -1,15 +1,20 @@
-#pragma once
-
 /*
+    Gintaras Grebliunas
+    combinacijus@gmail.com
+
     Library for converting current sensor data to current in miliamps
     Calibration with constructor:
         Set _mid to sensor value when no current
         Set _dir to -1 or 1 if you get negative current
         _calib used for calibrating output by scaling it(1.5 would mean output multiplied by 1.5)
+        _offset to offset value in mA
     How to use:
         Periodically call update() function to read and update sensor values
-        current: public value of the current
+        current: public value of the current (which can be computed with calibrate())
 */
+
+#pragma once
+
 class CurrentSensor
 {
 private:
@@ -24,9 +29,12 @@ private:
     float calib_mult; // Calibration multiplyer if sensor (if on by 100% set calib_mult to 2)
     float current = 0; // Current in miliamperes
 
+    float clamp(float x, float min, float max);
+
 public:
 
     CurrentSensor(int _pin, int _dir = -1, float _calib = 2.3, int _mid = 512, int _offset = 0);
+    void calibrate();
     void update();
     float get_current();
 };
