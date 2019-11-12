2c640fc4bb32d9a117cf715088e5916eaf0d228b
Combinacijus <combinacijus@gmail.com>
2019-11-12 15:56:59 +0200

    Working encoder reader prototype

--- Arduino/odometry_node/odometry_node.ino
+++ Arduino/odometry_node/odometry_node.ino
@@ -2,84 +2,127 @@
   Gintaras Grebliunas
   combinacijus@gmail.com
 
-  Odometry ROS node 
+  Odometry ROS node
+  CW
+  A 11001100
+  B 01100110
+  + 13201320
+  A+2B
 */
 
-int pin_a, pin_b;
-long position = 0;
-float speed = 0;
-int state_a;
-int state_a_old;
+int pin_a = 6;
+int pin_b = 7;
 unsigned long last_update_time = 0;
+
+int counter = 0;
+byte curr_state = 0b00000000;
+byte last_state = 0b00000000;
+
+volatile int value = 0;
 
 void setup()
 {
-  Serial.begin(115200);
-  delay(10);
-  Serial.print("Start");
-  
-  encoder_setup(A0, A1);
+	Serial.begin(115200);
+	//   delay(10);
+	Serial.print("Start");
 
+	encoder_setup();
 }
 
 void loop()
 {
-   static int c = 0;
-   encoder_update();
-   ++c;
-
-   if (c > 100)
-   {
-     Serial.println(position);
-     c = 0;
-   }
-
-//  Serial.print(analogRead(pin_a));
-//  Serial.print(" ");
-//  Serial.println(analogRead(pin_b));
-//  delay(1);
+	static int c = 0;
+	//  Serial.print(digitalRead(pin_a));
+	//	Serial.println(value);
+	Serial.println(counter);
+	delay(150);
 }
 
-void encoder_setup(int _pin_a, int _pin_b)
+void encoder_setup()
 {
-	pin_a = _pin_a;
-	pin_b = _pin_b;
+	pinMode(pin_a, INPUT_PULLUP);
+	pinMode(pin_b, INPUT_PULLUP);
 
-	pinMode(pin_a, INPUT);
-	pinMode(pin_b, INPUT);
+	cli();
+	// Pin change interrupt register for ports
+	PCICR |= 0b00000001; // turn on port b  13-8
+						 //	PCICR |= 0b00000010;    // turn on port c  A5-A0
+	PCICR |= 0b00000100; // turn on port d  7-0(2)
+
+	// Pin change mask for pins of the port
+	PCMSK0 |= 0b11111111; // 13-8
+	PCMSK1 |= 0b11111111; // A5-A0
+	PCMSK2 |= 0b11111100; // 7-0(2)
+	sei();
 }
 
-void encoder_update()
+// PortB interrupt
+ISR(PCINT0_vect)
 {
-	//state_a = digitalRead(pin_a); // Reads the "current" state of the outputA
-  state_a = analogRead(pin_a) > 300? 1 : 0; // Reads the "current" state of the outputA
-	// If the previous and the current state of the outputA are different, that means a Pulse has occured
-	if (state_a != state_a_old)
-	{
-		speed = 1000000.0 / (micros() - last_update_time);
-		// If the A state is different to the B state, that means the encoder is rotating clockwise
-    int state_b = analogRead(pin_b) > 300? 0 : 1;
-//    Serial.println(String(state_a) + " " + String(state_b));
-		if (state_b != state_a)
-		{
-			++position;
-		}
-		else
-		{
-			--position;
-			speed = -speed;
-		}
-		//Serial.print("Enc ");
-		//Serial.println(position);
+}
 
-		//Serial.println(micros() - last_update_time);
-		last_update_time = micros();
-		//Serial.println("speed " + String(speed));
-	}
-	else
+// PortD interrupt
+ISR(PCINT2_vect)
+{
+	//	CW
+	//  A 11001100
+	//  B 01100110
+	//  + 13201320
+	// D6 and D7 PIND
+	// 0 64 192 128 0 64 192 128
+
+	last_state = curr_state;
+	curr_state = PIND & 0b11000000; // D7 and D6
+
+	if (last_state == 0)
 	{
-//		speed *= 0.95;
+		if (curr_state == 64)
+			++counter;
+		else if (curr_state == 128)
+			--counter;
 	}
 
-	state_a_old = state_a; // Updates the previous state of the outputA with the current state
+	if (last_state == 64)
+	{
+		if (curr_state == 192)
+			++counter;
+		else if (curr_state == 0)
+			--counter;
+	}
+
+	if (last_state == 192)
+	{
+		if (curr_state == 128)
+			++counter;
+		else if (curr_state == 64)
+			--counter;
+	}
+
+	if (last_state == 128)
+	{
+		if (curr_state == 0)
+			++counter;
+		else if (curr_state == 192)
+			--counter;
+	}
+
+	// if (last_state ^ curr_state < 0b00000011)
+	// {
+	// 	// Serial.println("g");
+	// }
+	// else
+	// {
+	// 	// Serial.println("bad");
+	// 	// Serial.println("a" + String(a));
+	// 	// Serial.println("b" + String(b));
+	// 	// Serial.println("ls" + String(last_state));
+	// }
+	// Serial.println("cs" + String(curr_state));
 }
+
+// PortC interrupt
+// ISR(PCINT1_vect)
+// {
+//  Serial.print("C");
+//  Serial.println(PINC);
+// }
