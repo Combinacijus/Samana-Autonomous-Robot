0b0c657f9e225cfd5a090060c25c16344622984b
Combinacijus <combinacijus@gmail.com>
2019-12-22 12:16:14 +0200

    Started STM32 code

--- Arduino/odometry_node/odometry_node.ino
+++ Arduino/odometry_node/odometry_node.ino
@@ -19,12 +19,21 @@
 	TODO: to handle counter overflow (mess up speed)
 */
 
+#include <ros_odom.h>
+#include <samana_msgs/OdometrySmall.h>
+
+
+ros::NodeHandle nh;
+samana_msgs::OdometrySmall odometry_msg;
+ros::Publisher odometry_pub("odom", &odometry_msg);
+const int BAUD_RATE = 115200;
+
 int pin_a = 6;
 int pin_b = 7;
 unsigned long last_update_time = 0;
 
-volatile int counter1 = 0;
-volatile int counter2 = 0;
+volatile int ticks1 = 0;
+volatile int ticks2 = 0;
 
 const float ticks_to_rot = 1000000.0 / 2400.0;
 
@@ -38,18 +47,20 @@ void setup()
 	PORTC |= 0b11111111; // Pullup
 	PORTD |= 0b11111100; // Pullup leave last bits because it's TX and RX
 
-	Serial.begin(115200);
-	Serial.print("Start");
+	// Serial.begin(115200);
+	// Serial.print("Start");
 
-	encoder_setup();
+	encoderSetup();
+
+	initROS();
 }
 
 void loop()
 {
 	static unsigned long last_time = 0;
 	static unsigned long dt = -1;
-	static unsigned int last_count1 = 0;
-	static unsigned int last_count2 = 0;
+	static unsigned int last_ticks1 = 0;
+	static unsigned int last_ticks2 = 0;
 	static long dc1 = 0;
 	static long dc2 = 0;
 	static float speed1 = 0;
@@ -60,24 +71,31 @@ void loop()
 
 	dt = micros() - last_time;
 	last_time = micros();
-	dc1 = counter1 - last_count1;
-	last_count1 = counter1;
-	dc2 = counter2 - last_count2;
-	last_count2 = counter2;
+	dc1 = ticks1 - last_ticks1;
+	last_ticks1 = ticks1;
+	dc2 = ticks2 - last_ticks2;
+	last_ticks2 = ticks2;
 
 	speed1 = dc1 * ticks_to_rot / dt;
 	speed2 = dc2 * ticks_to_rot / dt;
-	Serial.println("A" + String(speed1));
-	Serial.println("B" + String(speed2));
 
-	Serial.println("A" + String((long)last_count1));
-	Serial.println("B" + String((long)last_count2));
-	Serial.println("");
+	// Serial.println("A" + String(speed1));
+	// Serial.println("B" + String(speed2));
+	// Serial.println("A" + String((long)last_count1));
+	// Serial.println("B" + String((long)last_count2));
+	// Serial.println("");
 
-	delay(150);
+	odometry_msg.ticks1 = last_ticks1;
+	odometry_msg.ticks2 = last_ticks2;
+	odometry_msg.speed1 = speed1;
+	odometry_msg.speed2 = speed2;
+	odometry_pub.publish(&odometry_msg);
+	nh.spinOnce();
+
+	delay(100);
 }
 
-void encoder_setup()
+void encoderSetup()
 {
 	cli();
 	// NOTE: hardcoded
@@ -123,7 +141,7 @@ ISR(PCINT0_vect)
 	last_state = curr_state;
 	curr_state = (PINB & 0b0000011); // Read D9 and D8. NOTE: hardcoded
 	// Serial.println(curr_state);
-	counter1 += decision_matrix[last_state][curr_state];
+	ticks1 += decision_matrix[last_state][curr_state];
 }
 
 // PortD interrupt. Max speed - 38 rotations per second
@@ -155,5 +173,25 @@ ISR(PCINT2_vect)
 	last_state = curr_state;
 	curr_state = (PIND & 0b11000000) >> 6; // Read D7 and D6 and push to LSB. NOTE: hardcoded
 	// Serial.println(curr_state);
-	counter2 += decision_matrix[last_state][curr_state];
+	ticks2 += decision_matrix[last_state][curr_state];
+}
+
+/*
+    Inits node, advertises topics
+*/
+void initROS()
+{
+    nh.getHardware()->setBaud(BAUD_RATE);
+    nh.initNode();
+
+    nh.advertise(odometry_pub);
+
+    while (!nh.connected()) // Wait until connected
+    {
+        nh.spinOnce();
+    }
+
+    // Debuging parameters. Brake if not enough dynamic memory
+    // sprintf(log_msg, "%d", recalibrate);
+    // nh.logwarn(log_msg);
 }
