46e68880bbd71cbd6ba51edb13e22ca4ae2440cc
Combinacijus <combinacijus@gmail.com>
2020-03-05 16:54:32 +0200

    Goldbag minor changes

--- Python/GoldBagDetector/GoldBagDetector.py
+++ Python/GoldBagDetector/GoldBagDetector.py
@@ -9,13 +9,14 @@
 """
 
 from imageai.Detection.Custom import CustomObjectDetection
-import cv2
+import cv2, time
 
 DIR_MODEL = "detection_model-ex-58--loss-1.79.h5"
 DIR_CONFIG = "detection_config.json"
 
 # Find usb webcam
-# cap = None
+cap = None
+cap_i = 0
 for i in range(1, 11):
     try:
         print(i)
@@ -23,10 +24,10 @@ for i in range(1, 11):
         _, frame = cap.read()
         cv2.imshow("aa", frame)
         cv2.destroyAllWindows()
+        cap_i = i
         break
     except Exception:
         pass
-
 
 detector = CustomObjectDetection()
 detector.setModelTypeAsYOLOv3()
@@ -34,36 +35,46 @@ detector.setJsonPath(DIR_CONFIG)
 detector.setModelPath(DIR_MODEL)
 detector.loadModel()
 
+# cap2 = VideoCapture2(cap_i)
+
+start_t = time.time()
 num = 1
 while cv2.waitKey(1) != ord('q'):
-    print("frame", num)
+    # Timming
+    end_t = time.time()
+    dt = end_t - start_t
+    debug_str = "Frame: %2d   dt:%4f   FPS: %2.3f" % (num, dt, 1 / dt)
+    print(debug_str)
+    start_t = time.time()
     num += 1
-    _, frame = cap.read()
-    # frame = cv2.resize(frame, (244, 157))
-    # detections = detector.detectObjectsFromImage(input_type="array", input_image=frame,
-    #                                              output_image_path="image2new.jpg",
-    #                                              minimum_percentage_probability=50)
 
-    print("before")
-    frame_out, detections = detector.detectObjectsFromImage(input_type="array", input_image=frame,
-                                                            output_type="array", minimum_percentage_probability=50)
-    for eachObject in detections:
-        print(eachObject["name"], " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"])
-    print("after")
+    try:
+        for i in range(7):  # Cleans buffer and reads current frame
+            _, frame = cap.read()  # Get new frame from camera
 
-    # font
+        # detections = detector.detectObjectsFromImage(input_type="array", input_image=frame,
+        #                                              output_image_path="image2new.jpg",
+        #                                              minimum_percentage_probability=50)
+        frame_out, detections = detector.detectObjectsFromImage(input_type="array", input_image=frame,
+                                                                output_type="array", minimum_percentage_probability=1)
+        for det in detections:
+            print(det["name"], " : ", det["percentage_probability"], " : ", det["box_points"])
+    except Exception:
+        num -= 1
+        print("Error in detection. Skipping frame")
+        break
+
+    # Debug text on image
     font = cv2.FONT_HERSHEY_SIMPLEX
-
-    org = (50, 50)
-    fontScale = 3
+    org = (5, 20)
+    fontScale = 0.4
     color = (255, 0, 0)
-    # Line thickness of 2 px
-    thickness = 2
-    frame = cv2.putText(frame, 'OpenCV', org, font,
-                        fontScale, color, thickness, cv2.LINE_AA)
+    thickness = 1
+    frame_out = cv2.putText(frame_out, debug_str, org, font,
+                            fontScale, color, thickness, cv2.LINE_AA)
 
     cv2.imshow("Gold bag detector", frame_out)
-    # cv2.imshow("Gold bag detector", frame)
+    # cv2.imwrite("file%d.jpg" % num, frame_out)
 
 cap.release()
 cv2.destroyAllWindows()
