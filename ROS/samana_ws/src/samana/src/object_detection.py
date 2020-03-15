37a536f4a873337cd68e7f513b8ef2a18d055b0e
Combinacijus <combinacijus@gmail.com>
2020-03-16 00:01:42 +0200

    Basic ROS integration. debug_image service

--- ROS/samana_ws/src/samana/src/object_detection.py
+++ ROS/samana_ws/src/samana/src/object_detection.py
@@ -7,6 +7,8 @@ Send out message to TCP/IP client to read camera and detect object
 Gets message from client about detected objectss and their position
 Transforms screen position to world frame
 
+NOTE: If socket in use run   lsof -i :5005   and check listening processes
+
 Read NOTE: for important places in code
 '''
 
@@ -15,11 +17,13 @@ import socket
 import pickle
 import time
 import timeout_decorator
-import cv2 as cv
+# import cv2 as cv
 from subprocess import Popen
-# from std_msgs.msg import UInt8
+from std_srvs.srv import SetBool, SetBoolResponse, SetBoolRequest
 
 TIMEOUT = 2  # Detection timeout
+IMG_W = 640
+IMG_H = 480
 
 class TcpServer:
     '''
@@ -34,9 +38,13 @@ class TcpServer:
         self.BUFFER_SIZE = 1024
 
         # Init server
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.bind((self.TCP_IP, self.TCP_PORT))
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.socket.bind((self.TCP_IP, self.TCP_PORT))
+        except Exception:
+            print("Close image viewer window")
+            raise
 
         self.wait_for_client()
 
@@ -71,22 +79,21 @@ class TcpServer:
 
 class DetectionParser:
     def __init__(self):
-        self.debug_image = True  # NOTE: For seeing detected object image
         self.server = TcpServer()
         self.pickled_data = None
         self.viewer = None
 
-    def send_request(self):
+    def send_request(self, debug_image=False):  # NOTE: For seeing detected object image
         '''
             Request contains data of how many seconds are given before timeout
             Also for simplicity if timeout >=1000 it means to enable debug_image
         '''
         msg = TIMEOUT
-        if self.debug_image is True:  # Tell client to enable debug_image
+        if debug_image is True:  # Tell client to enable debug_image
             msg += 1000
         msg = pickle.dumps(msg)
         self.server.send(msg)
-        print("SENT: timeout: {}  debug_img: {}".format(TIMEOUT, self.debug_image))
+        print("SENT: timeout: {}  debug_img: {}".format(TIMEOUT, debug_image))
 
     def receive_data(self):
         '''
@@ -97,6 +104,8 @@ class DetectionParser:
         '''
         try:
             self.pickled_data = self.server.receive_data()
+        except KeyboardInterrupt:  # BUG: won't catch
+            raise
         except Exception as e:
             print("ERROR: TIMEOUT! FAILED TO RECEIVE DETECTION.")
             print("CHECK SOCKET CONNECTION, TIMEOUT SETTINGS, CAMERA CONNECTION")
@@ -105,18 +114,19 @@ class DetectionParser:
             self.pickled_data = None
         return self.pickled_data
 
-    def data_debug(self):
+    def data_debug(self, debug_image=False):
         if self.pickled_data:
             detection = pickle.loads(self.pickled_data)
             if detection:
-                print("{d[name]} : {d[percentage_probability]} : {d[box_points]}".format(d=detection))
+                print("{d[name]} : {d[percentage_probability]} : {d[box_points]}".format(
+                    d=detection))
             else:
                 print("No detections")
         else:
             print("No detections")
-        
+
         # Display image
-        if self.debug_image is True:
+        if debug_image is True:
             if not self.viewer:
                 # NOTE: path to the image
                 self.viewer = Popen(["eog", "/home/combinacijus/Documents/SamanaAutonomousRobot/Python/GoldBagDetector/image.jpg"])
@@ -127,20 +137,76 @@ class DetectionParser:
             except Exception:
                 pass
 
+
+class RosHandler():
+    def __init__(self, debug_image=False):
+        self.debug_image = debug_image
+        self.init_ros()
+
+    def init_ros(self):
+        rospy.init_node('yolo_detector', anonymous=True)
+
+        # Publishers
+        # self.teleop_pub = rospy.Publisher('teleop', Teleop, queue_size=10)
+
+        # Messages
+        # self.cmd_vel = Twist()
+
+        # Subscribers
+        # rospy.Subscriber("rc/modes", RCModes, self.rc_modes_calback)
+
+        # Services
+        rospy.Service("set_debug_image", SetBool, self.handle_set_debug_image)
+
+        # Infinite loop
+        # self.control_motors()
+
+    def handle_set_debug_image(self, req):
+        '''
+            Service callback function for setting debug_image variable
+        '''
+        if isinstance(req, SetBoolRequest):
+            self.debug_image = req.data
+            return SetBoolResponse(True, '')
+        return SetBoolResponse(False, 'Wrong data type')
+
+
 if __name__ == '__main__':
+    rosh = RosHandler(True)  # ROS handler
     dparser = DetectionParser()
 
     while True:
         try:
             print("---------------------------")
-            dparser.send_request()
 
-            # Receive data
+            dparser.send_request(rosh.debug_image)
             pickled_data = dparser.receive_data()
-            if not pickled_data:
+            if not pickled_data:  # No data received
                 continue
-            
-            dparser.data_debug()
+            dparser.data_debug(rosh.debug_image)
+
+            data = pickle.loads(pickled_data)
+            if data:  # Something detected TODO: continue work
+                box = data["box_points"]
+                prob = data["percentage_probability"]
+                print("box {}".format(box))
+                
+                cx = (box[0] + box[2]) / 2.0
+                cy = (box[1] + box[3]) / 2.0
+                dx = (box[2] - box[0])
+                dy = (box[3] + box[1])
+                center = [cx / IMG_W, cy / IMG_H]  # In unit 
+                lower10 = [cx / IMG_W, cy + 0.45*dy]  # In unit. 10% above the bottom so it point more to the base of object
+                print("center ({:.2f}, {:.2f}   with prob: {:.2f})".format(center[0], center[1], prob))
+                print("center ({:.2f}, {:.2f}".format(lower10[0], lower10[1]))  # TODO: check if works
+                # TODO show points in opencv
+                # TODO Project camera space to ground plane
+            else:  # No detection
+                pass
+
+        # Ctrl + C (doesn't work because dparser.receive_data)
         except KeyboardInterrupt:
             print("Program finished")
             break
+        except Exception as e:
+            print("HOPE NO CRASH: ", e)
