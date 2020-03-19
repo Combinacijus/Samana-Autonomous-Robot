39159781706d49d90f48353ff3dfca2613bac7ed
Combinacijus <combinacijus@gmail.com>
2020-03-20 00:26:47 +0200

    Bug fixes with bad behaviour when odometry is lost; refractoring

--- ROS/samana_ws/src/samana/src/motors_controller.py
+++ ROS/samana_ws/src/samana/src/motors_controller.py
@@ -19,6 +19,9 @@ from std_msgs.msg import String
 from std_msgs.msg import Float64
 from std_msgs.msg import Bool
 from nav_msgs.msg import Odometry
+from std_srvs.srv import SetBool, SetBoolRequest
+from robot_localization.srv import ToggleFilterProcessing, ToggleFilterProcessingRequest
+from helpers import IsFresh
 
 
 class MotorsController:
@@ -30,16 +33,19 @@ class MotorsController:
         self.MAX_ACCEL_ANG = max(0, rospy.get_param("hoverboard/max_accel_ang", 400))
         self.CMD_VEL_TIMEOUT = rospy.Duration(0.15)
         self.RC_TELEOP_TIMEOUT = rospy.Duration(0.07)
+        self.ODOM_TIMEOUT = rospy.Duration(0.04)
         self.SWITCH_TRIG = 950  # Trigger value for a switch
         self.TELEOP_RATE = 40  # Command sending to motor frequency
 
-        self.allow_rc = False
-        self.armed = False  # When false ignore RC
-        self.auton_mode = False  # When True read /cmd_vel
-        self.last_cmd_vel_stale = True  # When True don't use cmd_vel
-        self.last_cmd_vel_update = rospy.Time()
-        self.last_rc_teleop_stale = False  # When True don't use rc_teleop
-        self.last_rc_teleop_update = rospy.Time()
+        # Variables
+        self.allow_rc = False    # When false always ignore RC
+        self.armed = False       # When false ignore RC
+        self.auton_mode = False  # When True read /cmd_vel and publish it to PID controller
+
+        # Is fresh variables
+        self.fresh_cmd_vel = IsFresh(0.15, "Velocity command")
+        self.fresh_rc_teleop = IsFresh(0.15, "Remote teleop")
+        self.fresh_odom = IsFresh(0.1, "Odometry")
 
     def init_ros(self):
         rospy.init_node('motors_controller', anonymous=True)
@@ -52,7 +58,7 @@ class MotorsController:
         self.setpoint_yaw_pub = rospy.Publisher('yaw_pid/setpoint', Float64, queue_size=1)
         self.state_yaw_pub = rospy.Publisher('yaw_pid/state', Float64, queue_size=1)
         self.pid_yaw_enable_pub = rospy.Publisher('yaw_pid/pid_enable', Bool, queue_size=1, latch=True)
-        self.pub_audio = rospy.Publisher('text_to_speech', String, queue_size=5)
+        self.audio_pub = rospy.Publisher('text_to_speech', String, queue_size=5)
 
         # Messages
         self.cmd_vel = Twist()
@@ -71,6 +77,9 @@ class MotorsController:
         rospy.Subscriber("odom", Odometry, self.odom_callback)
         rospy.Subscriber("vel_pid/control_effort", Float64, self.pid_vel_callback)
         rospy.Subscriber("yaw_pid/control_effort", Float64, self.pid_yaw_callback)
+
+        # Services
+        self.toggle_ekf_srv = rospy.ServiceProxy("/ekf_odom/toggle", ToggleFilterProcessing)
 
         # Infinite loop
         self.control_motors()
@@ -108,14 +117,14 @@ class MotorsController:
             Updates rc_teleop
         '''
         self.rc_teleop = data
-        self.last_rc_teleop_update = rospy.Time.now()  # [BUG] simple rospy.Time() returns 0
+        self.fresh_rc_teleop.updated()
 
     def cmd_vel_callback(self, cmd_vel):
         '''
             Publish velocity and yaw speed setpoint for a pid controller if auton_mode is true
             Save cmd_vel to a variable for later use in control funciton
         '''
-        self.last_cmd_vel_update = rospy.Time.now()
+        self.fresh_cmd_vel.updated()
         self.cmd_vel = cmd_vel
 
         if self.auton_mode is True:
@@ -130,6 +139,7 @@ class MotorsController:
     def odom_callback(self, odom):
         '''
             Publishes state data for PID controller
+            Updates that fresh odometry received
         '''
         # State for velocity PID
         self.float64.data = odom.twist.twist.linear.x
@@ -138,6 +148,9 @@ class MotorsController:
         # State for yaw speed PID
         self.float64.data = odom.twist.twist.angular.z
         self.state_yaw_pub.publish(self.float64)
+
+        # Fresh odometry received
+        self.fresh_odom.updated()
 
     def pid_vel_callback(self, effort):
         '''
@@ -191,78 +204,90 @@ class MotorsController:
         # If faster update rate is needed try higher baud rate for hoverboard serial
         rate = rospy.Rate(self.TELEOP_RATE)  # Recommended 40Hz
         while not rospy.is_shutdown():  # ---------------------------Infinite loop
-            if self.allow_rc is True and self.armed is True:  # -----RC control
-                # Check if rc_teleop is not stale
-                if rospy.Time.now() - self.last_rc_teleop_update > self.RC_TELEOP_TIMEOUT:
-                    if self.last_rc_teleop_stale is not True:  # Audio feedback
-                        self.pub_audio.publish("Teleop command stale")
-                        self.last_rc_teleop_stale = True
-                    # Zero rc_teleop
-                    self.rc_teleop.speed = 0
-                    self.rc_teleop.steer = 0
-                    self.enable_pids(False)
-                else:  # rc_teleop not stale
-                    self.last_rc_teleop_stale = False
-                    self.enable_pids(True)
+            # -------------------------------- SAFETY ---------------------------------------
+            # Disable EKF filter when no odometry available and enable if it's fresh
+            # Disable any control because PID requires odometry and if it fails it will go at full speed
+            if self.fresh_odom.changed() is True:  # On odom freshes change
+                is_fresh = self.fresh_odom.is_fresh()
+                req = ToggleFilterProcessingRequest(is_fresh)
+                self.toggle_ekf_srv(req)
+                self.enable_pids(is_fresh)
 
-                # self.teleop = self.rc_teleop  # Old pure RC control
+                if self.fresh_odom.is_fresh() is False:
+                    self.audio_pub.publish("Critical drive disabled")
+                else:
+                    self.audio_pub.publish("Drive enabled")
 
-                # --------------------------- PID RC CONTROL ---------------------------
-                # Sub and Publish state vel to state_vel topic (in odom_callback())
-                
-                # Velocity setpoint in m/s
-                vel = self.rc_teleop.speed / 1000.0 * 2.0  # Maximum sensible 3.7 m/s
-                self.float64.data = vel
-                self.setpoint_vel_pub.publish(self.float64)
+            if self.fresh_odom.is_fresh() is False:
+                # Stop hoverboard don't allow any movement until odometry data is updated
+                self.teleop.speed = 0
+                self.teleop.steer = 0
+            else:  # Everything ok. Continue with normal control
+                # ------------------------------- CONTROL STATES --------------------------------
+                if self.allow_rc is True and self.armed is True:  # ---------RC control
+                    # Check if rc_teleop is not stale
+                    if self.fresh_rc_teleop.is_fresh() is True:
+                        self.enable_pids(True)
+                    else:  # rc_teleop stale
+                        # Zero rc_teleop
+                        self.rc_teleop.speed = 0
+                        self.rc_teleop.steer = 0
+                        self.enable_pids(False)
 
-                # Yaw speed setpoint
-                spin = self.rc_teleop.steer / 1000.0 * 6.28  # Maximum sensible 7 rad/s
-                self.float64.data = spin
-                self.setpoint_yaw_pub.publish(self.float64)
+                    # self.teleop = self.rc_teleop  # Old pure RC control
 
-                # These are updated in callback function
-                self.teleop.speed = self.pid_teleop.speed
-                self.teleop.steer = self.pid_teleop.steer
+                    # --------------------------- PID RC CONTROL ---------------------------
+                    # Sub and Publish state vel to state_vel topic (in odom_callback())
 
-                limit_accel()  # Limit acceleration
+                    # Velocity setpoint in m/s
+                    vel = self.rc_teleop.speed / 1000.0 * 2.0  # Maximum sensible 3.7 m/s
+                    self.float64.data = vel
+                    self.setpoint_vel_pub.publish(self.float64)
 
-            elif self.auton_mode is True:  # ----------- Autonomous control
-                '''
-                    Brief: Executes /cmd_vel commands
-                    Reads /cmd_vel in callback
-                    Uses PID to find apropriate teleop commands
-                    Sets teleop commands
-                '''
+                    # Yaw speed setpoint
+                    spin = self.rc_teleop.steer / 1000.0 * 6.28  # Maximum sensible 7 rad/s
+                    self.float64.data = spin
+                    self.setpoint_yaw_pub.publish(self.float64)
 
-                self.enable_pids(True)
-                # Check if /cmd_vel is stale and should not be used
-                if rospy.Time.now() - self.last_cmd_vel_update > self.CMD_VEL_TIMEOUT:  # cmd_vel is stale
-                    if self.last_cmd_vel_stale is not True:
-                        self.pub_audio.publish("Velocity command stale")  # Audio feedback
-                    self.last_cmd_vel_stale = True
-
-                    self.teleop.speed = 0
-                    self.teleop.steer = 0
-                else:  # cmd_vel is good
-                    '''
-                        Do only if auton_mode is True (set inside cmd_vel_callback function)
-                        Publish cmd_vel velocity as setpoint for pid controller (cmd_vel_callback())
-                        Sub and Publish state vel to state_vel topic (odom_callback())
-                        Sub to effort_vel topic (pid_vel_callback())
-                        Set effort to teleop
-                    '''
-
-                    self.last_cmd_vel_stale = False
+                    # These are updated in callback function
                     self.teleop.speed = self.pid_teleop.speed
                     self.teleop.steer = self.pid_teleop.steer
 
-                limit_accel()  # Limit acceleration from pid linear and angular
+                    limit_accel()  # Limit acceleration
 
-                # print('Auton mode: speed: {:2.2f}  steer: {:2.2f}'.format(self.teleop.speed, self.teleop.steer))
-            else:  # ------------------------------------------------ Fully disarmed no control
-                self.teleop.speed = 0
-                self.teleop.steer = 0
-                self.enable_pids(False)
+                elif self.auton_mode is True:  # ----------- Autonomous control
+                    '''
+                        Brief: Executes /cmd_vel commands
+                        Reads /cmd_vel in callback
+                        Uses PID to find apropriate teleop commands
+                        Sets teleop commands
+                    '''
+
+                    self.enable_pids(True)
+
+                    # Check if /cmd_vel is stale and should not be used
+                    if self.fresh_cmd_vel.is_fresh() is True:
+                        '''
+                            Do only if auton_mode is True (set inside cmd_vel_callback function)
+                            Publish cmd_vel velocity as setpoint for pid controller (cmd_vel_callback())
+                            Sub and Publish state vel to state_vel topic (odom_callback())
+                            Sub to effort_vel topic (pid_vel_callback())
+                            Set effort to teleop
+                        '''
+
+                        self.teleop.speed = self.pid_teleop.speed
+                        self.teleop.steer = self.pid_teleop.steer
+                    else:  # cmd_vel is stale
+                        self.teleop.speed = 0
+                        self.teleop.steer = 0
+
+                    limit_accel()  # Limit acceleration from pid linear and angular
+
+                    # print('Auton mode: speed: {:2.2f}  steer: {:2.2f}'.format(self.teleop.speed, self.teleop.steer))
+                else:  # ------------------------------------------------ Fully disarmed no control
+                    self.teleop.speed = 0
+                    self.teleop.steer = 0
+                    self.enable_pids(False)
 
             # Global clamp
             self.teleop.speed = self.clamp(self.teleop.speed, -self.MAX_SPEED, self.MAX_SPEED)
