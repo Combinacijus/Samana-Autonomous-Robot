6c92896ef2f22660272c8e026e0180ab4af36029
Combinacijus <combinacijus@gmail.com>
2020-08-20 14:02:08 +0300

    Kill all nodes helper

--- ROS/samana_ws/src/samana/src/kill_all_nodes.py
+++ ROS/samana_ws/src/samana/src/kill_all_nodes.py
@@ -1,10 +1,44 @@
+#!/usr/bin/env python
 """
     Sometimes nodes get stuck running in background even after roscore is terminated
     In this script are written all nodes that were catched not terminating properly
     And those are killed
 """
 
-import os
+import re
+import psutil
 
-os.system("killall move_base")
-os.system("killall static_transform_publisher")
+
+count = 0
+print("KILLING THESE ROS PROCESSES:")
+
+# Samana python nodes
+for proc in psutil.process_iter():
+    for line in proc.cmdline():
+        LINE1 = '/home/combinacijus/Documents/SamanaAutonomousRobot/ROS/samana_ws/'
+        if re.search(LINE1, line) and proc.cmdline()[0] == 'python' and not re.search("kill_all_nodes.py", line):
+            print(proc.cmdline())
+            proc.kill()
+            count += 1
+
+# ROS library nodes
+for proc in psutil.process_iter():
+    for line in proc.cmdline():
+        LINE1 = '/opt/ros/melodic/lib'
+        # LINE2 = '/opt/ros/melodic/bin/roscore'
+        # LINE3 = '/opt/ros/melodic/bin/rosmaster'
+        # print(proc.cmdline())
+        if re.search(LINE1, line): # or re.search(LINE2, line) or re.search(LINE3, line):
+            print(proc.cmdline())
+            proc.kill()
+            count += 1
+
+print("TOTAL PROCESSES KILLED: {}".format(count))
+
+# ROS library nodes
+for proc in psutil.process_iter():
+    for line in proc.cmdline():
+        LINE1 = '/opt/ros/melodic/bin/roscore'
+        LINE2 = '/opt/ros/melodic/bin/rosmaster'
+        if re.search(LINE1, line)or re.search(LINE2, line):
+            proc.kill()
