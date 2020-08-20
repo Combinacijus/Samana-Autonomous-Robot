f4909e95e9a5d8d44ebf805eb14f5ab4ac00566c
Combinacijus <combinacijus@gmail.com>
2020-08-20 14:14:15 +0300

    Arduino libraries soft links refactoring

--- Arduino/make_links.sh
+++ Arduino/make_links.sh
@@ -1,7 +1,12 @@
-echo "Creating soft link from ros_* to my default Arduino libraries"
-ln -sv /home/combinacijus/Documents/SamanaAutonomousRobot/Arduino/libraries/ros_lib/ /home/combinacijus/Documents/Arduino/libraries/
-ln -sv /home/combinacijus/Documents/SamanaAutonomousRobot/Arduino/libraries/ros_lib_sonar/ /home/combinacijus/Documents/Arduino/libraries/
-ln -sv /home/combinacijus/Documents/SamanaAutonomousRobot/Arduino/libraries/ros_lib_imu/ /home/combinacijus/Documents/Arduino/libraries/
-ln -sv /home/combinacijus/Documents/SamanaAutonomousRobot/Arduino/libraries/ros_lib_motor/ /home/combinacijus/Documents/Arduino/libraries/
-ln -sv /home/combinacijus/Documents/SamanaAutonomousRobot/Arduino/libraries/ros_lib_odom/ /home/combinacijus/Documents/Arduino/libraries/
-ln -sv /home/combinacijus/Documents/SamanaAutonomousRobot/Arduino/libraries/ros_lib_arm/ /home/combinacijus/Documents/Arduino/libraries/
+#!/bin/bash
+echo "Creating soft link from ros_lib_* to my default Arduino libraries"
+
+lib_path_samana="/home/combinacijus/Documents/SamanaAutonomousRobot/Arduino/libraries/"
+lib_path_arduino="/home/combinacijus/Documents/Arduino/libraries/"
+
+ln -sv "${lib_path_samana}ros_lib/" 			"$lib_path_arduino"
+ln -sv "${lib_path_samana}ros_lib_sonar/" 		"$lib_path_arduino"
+ln -sv "${lib_path_samana}ros_lib_imu/" 		"$lib_path_arduino"
+ln -sv "${lib_path_samana}ros_lib_motor/" 		"$lib_path_arduino"
+ln -sv "${lib_path_samana}ros_lib_gps_odom/" 	"$lib_path_arduino"
+ln -sv "${lib_path_samana}ros_lib_arm/" 		"$lib_path_arduino"
